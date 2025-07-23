aee9a990900c5e549545b318dc6812115f8b97d1
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2025-07-23 15:01:44 +0100

    Créatiion d'un évènement pour signaler la fin d'une analyse.

---
+++ app/Events/PlagiarismAnalysisCompleted.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PlagiarismAnalysisCompleted implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $textAnalysisId; 
+    public $status;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(int $textAnalysisId, string $status)
+    {
+        $this->textAnalysisId = $textAnalysisId;
+        $this->status = $status;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel("plagiarism-analysis.$this->textAnalysisId"),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'analysis-completed';
+    }
+}
